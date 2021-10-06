556b16d77c9913367ea018311874e80267248696
Xie Yuheng <xyheme@gmail.com>
2021-10-07 00:05:56 +0800

    up

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -10,9 +10,9 @@
 
       <p>
         The aim of cicada project is to help people understand that developing
-        software and developing mathematics are the same kind of activity, and
-        people who practices these developments, can learn from each other, and
-        help each other in very good ways.
+        software and developing mathematics are increasingly the same kind of
+        activity, and people who practices these developments, can learn from
+        each other, and help each other in very good ways.
       </p>
     </div>
 
