74643ed3185e0f5a3a31e2aafacca1dab7adf550
Xie Yuheng <xyheme@gmail.com>
2021-10-08 02:24:24 +0800

    - remove `ut`

--- src/ut/freshen.test.ts
+++
@@ -1,6 +0,0 @@
-import { freshen } from "./freshen"
-import * as ut from "./index"
-
-ut.assertEqual("x1", freshen(new Set(["x"]), "x"))
-ut.assertEqual("x2", freshen(new Set(["x", "x1"]), "x"))
-ut.assertEqual("x3", freshen(new Set(["x", "x1", "x2"]), "x"))
