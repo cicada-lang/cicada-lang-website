62ccd1f62fe80a7be4a12ebefce1743ae7c25dd8
Xie Yuheng <xyheme@gmail.com>
2021-10-18 14:19:01 +0800

    up

--- src/views/errors/page-not-found.vue
+++ src/views/errors/page-not-found.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center max-w-3xl mx-auto">
+  <div class="flex flex-col max-w-3xl mx-auto">
     <div class="py-20">
       <h1 class="font-bold text-2xl font-sans">404</h1>
       <p class="text-xl">Page Not Found</p>
