3c7408babaf304ad6285adac0de09af343bbb5d0
Xie Yuheng <xyheme@gmail.com>
2021-12-24 04:36:07 +0800

    update vue/cli to update tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,10 @@
-const colors = require("tailwindcss/colors")
-
 module.exports = {
-  purge: ["./src/**/*.vue"],
+  content: ["./src/**/*.vue"],
   theme: {
     fontFamily: {
       sans: ["Linux Biolinum O", "sans-serif"],
       serif: ["Linux Libertine O", "serif"],
-      narration: [
-        "EBGaramond",
-        // "BodoniModa",
-        "serif",
-      ],
-      mono: [
-        // "Inconsolata LGC",
-        // "Terminus (TTF)",
-        "Fira Code",
-        // "DejaVu Sans Mono",
-        "monospace",
-      ],
-    },
-    extend: {
-      colors: {
-        gray: colors.warmGray,
-        orange: colors.orange,
-        amber: colors.amber,
-        lime: colors.lime,
-        emerald: colors.emerald,
-        teal: colors.teal,
-        cyan: colors.cyan,
-        sky: colors.sky,
-        violet: colors.violet,
-        fuchsia: colors.fuchsia,
-        rose: colors.rose,
-      },
-    },
-  },
-  variants: {
-    extend: {
-      opacity: ["disabled"],
-      backgroundColor: ["disabled"],
-      borderWidth: ["hover", "focus"],
+      mono: ["Fira Code", "monospace"],
     },
   },
 }
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,20 @@
-module.exports = {
+const { defineConfig } = require("@vue/cli-service")
+
+module.exports = defineConfig({
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        fs: false,
+        assert: require.resolve("assert"),
+        utils: require.resolve("util"),
+        path: require.resolve("path-browserify"),
+      },
+    },
+  },
   pwa: {
     themeColor: "#ffffff",
     msTileColor: "#ffffff",
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "#ffffff",
   },
-}
+})
