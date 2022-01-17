50608881d6595ded0993c817e7af4a40cbcb6fb1
Xie Yuheng <xyheme@gmail.com>
2022-01-17 23:35:52 +0800

    up

--- src/layouts/page-layout/PageLayout.vue
+++ src/layouts/page-layout/PageLayout.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex flex-col w-full font-serif text-xl">
     <div
-      class="w-full px-6 flex flex-col items-center shadow fixed bg-white md:opacity-90"
+      class="w-full px-4 flex flex-col items-center shadow fixed bg-white md:opacity-90"
     >
       <PageHeader class="max-w-5xl" />
     </div>
 
-    <div class="mt-28 px-6 w-full flex flex-col items-center">
+    <div class="mt-20 px-4 w-full flex flex-col items-center">
       <slot class="max-w-5xl" />
     </div>
   </div>
--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -1,23 +1,19 @@
 <template>
   <PageLayout>
-    <section class="py-4 space-y-2">
-      <p class="md:text-5xl text-3xl font-serif text-center">
-        Let's build a bridge between <br />
-        <span class="font-bold text-gray-800">writing software</span> and <br />
-        <span class="font-bold text-gray-800">doing mathematics</span>
-      </p>
-    </section>
+    <div class="md:pt-16 pt-8 pb-4 md:text-5xl text-3xl font-serif text-center">
+      Let's build a bridge between <br />
+      <span class="font-bold text-gray-800">writing software</span> and <br />
+      <span class="font-bold text-gray-800">doing mathematics</span>
+    </div>
 
-    <section class="py-4 space-y-2">
-      <p class="md:text-2xl text-lg text-center">
-        <span class="font-logo text-gray-900">Cicada Language</span>
-        is a dependently typed
-        <br />
-        <span class="font-bold text-gray-900"> programming language</span>
-        and an <br />
-        <span class="font-bold text-gray-900">interactive theorem prover</span>
-      </p>
-    </section>
+    <div class="py-4 md:text-2xl text-xl text-center">
+      <span class="font-logo text-gray-900">Cicada Language</span>
+      is a dependently typed
+      <br />
+      <span class="font-bold text-gray-900"> programming language</span>
+      and an <br />
+      <span class="font-bold text-gray-900">interactive theorem prover</span>
+    </div>
   </PageLayout>
 </template>
 
