62e16355a888d7e5091f977c37546d10a6f5269c
Xie Yuheng <xyheme@gmail.com>
2022-01-18 04:47:40 +0800

    up

--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -1,12 +1,23 @@
 <template>
   <PageLayout>
-    <div class="md:pt-10 pt-8 pb-4 md:text-5xl text-3xl font-serif text-center">
+    <div
+      class="
+        md:pt-10
+        pt-8
+        pb-4
+        md:text-5xl
+        sm:text-4xl
+        text-3xl
+        font-serif
+        text-center
+      "
+    >
       Let's build a bridge between <br />
       <span class="font-bold text-gray-800">writing software</span> and <br />
       <span class="font-bold text-gray-800">doing mathematics</span>
     </div>
 
-    <div class="py-4 md:text-2xl text-xl text-center">
+    <div class="py-4 md:text-2xl sm:text-xl text-lg text-center">
       <span class="font-logo text-gray-900">Cicada Language</span>
       is a dependently typed
       <br />
