9e22409dce6197b7fa24e600fe627f842c2ef5fc
Xie Yuheng <xyheme@gmail.com>
2022-01-19 02:21:52 +0800

    up

--- src/layouts/page-layout/PageLayout.vue
+++ src/layouts/page-layout/PageLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col w-full font-serif items-center text-xl">
     <div
-      class="w-full px-4 flex flex-col items-center shadow fixed bg-white md:opacity-90"
+      class="w-full px-4 flex flex-col items-center shadow fixed bg-white opacity-90"
     >
       <PageHeader class="max-w-5xl" />
     </div>
