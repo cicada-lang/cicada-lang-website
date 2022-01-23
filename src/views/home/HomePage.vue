97fb2190f40b04f54fb3a2dc4963b3cd110ff28e
Xie Yuheng <xyheme@gmail.com>
2022-01-23 15:52:17 +0800

    up

--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -19,16 +19,30 @@
 
     <HomePageBeProud class="my-8" />
 
-    <div class="pt-2 pb-20 md:text-4xl font-bold font-sans text-center">
-      <div class="text-3xl py-6 text-amber-500">Get Started</div>
+    <div class="space-y-10 pb-10">
+      <div class="md:text-4xl font-bold font-sans text-center">
+        <div class="text-3xl pb-6 text-amber-500">Get Started</div>
 
-      <router-link to="/docs" class="font-normal">
-        <button
-          class="border-2 py-3 px-6 bg-amber-400 hover:bg-amber-300 rounded-lg font-bold text-2xl border-amber-200 text-white"
-        >
-          DOCS
-        </button>
-      </router-link>
+        <router-link to="/docs" class="font-normal">
+          <button
+            class="border-2 py-3 px-6 bg-amber-400 hover:bg-amber-300 rounded-lg font-bold text-2xl border-amber-200 text-white"
+          >
+            DOCS
+          </button>
+        </router-link>
+      </div>
+
+      <div class="md:text-4xl font-bold font-sans text-center">
+        <div class="text-3xl pb-6 text-lime-500">Become a Sponsor</div>
+
+        <router-link to="/sponsors" class="font-normal">
+          <button
+            class="border-2 py-3 px-6 bg-lime-400 hover:bg-lime-300 rounded-lg font-bold text-2xl border-lime-200 text-white"
+          >
+            SPONSORS
+          </button>
+        </router-link>
+      </div>
     </div>
   </PageLayout>
 </template>
