1b44177fa7a8c14022c4dc371ba9c4df664d8ea9
Xie Yuheng <xyheme@gmail.com>
2022-09-05 20:50:19 +0800

    - move styles/ to assets/

--- TODO.md
+++ TODO.md
@@ -1,6 +1,4 @@
 - remove layouts/page-layout/index.ts
-- move styles/ to assets/
-
 - components/Link.vue
 - use components/Link.vue instead of RouterLink
 - use heroicon package
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import FloatingVue from 'floating-vue'
 import router from './router'
-import './styles/index.css'
+import './assets/styles/index.css'
 import 'floating-vue/dist/style.css'
 import App from './App.vue'
 
