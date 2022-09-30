a79604a8f4cc06a198fd5ad52afd194ea4faf316
Xie Yuheng <xyheme@gmail.com>
2022-09-30 16:35:07 +0800

    up

--- src/pages/playground/PlaygroundHeader.vue
+++ src/pages/playground/PlaygroundHeader.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { PlaygroundState as State } from './PlaygroundState'
 import PlaygroundToolbar from './PlaygroundToolbar.vue'
+import Link from '../../components/Link.vue'
 
 defineProps<{
   state: State
@@ -11,12 +12,12 @@ defineProps<{
   <div
     class="border-theme-300 bg-theme-200 flex items-center justify-between border-b px-3 py-2"
   >
-    <router-link
-      to="/playground"
+    <Link
+      href="/playground"
       class="text-theme-900 font-logo text-xl font-semibold"
     >
-      Playground
-    </router-link>
+      Cicada Playground
+    </Link>
     <PlaygroundToolbar :state="state" />
   </div>
 </template>
